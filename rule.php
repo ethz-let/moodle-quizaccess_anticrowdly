648cadbc77cc1ba7fb2428b2f43ac5ea2169d78b
Nexterday <nexterday@users.noreply.github.com>
2026-01-14 21:36:27 +0100

    Remove preview user check from description method
    Removed unnecessary check for preview user in description method.

--- rule.php
+++ rule.php
@@ -49,17 +49,12 @@ class quizaccess_antiai extends access_rule_base {
     
     public function description(): array {
         global $PAGE, $SESSION;
-
-        if ($this->quizobj->is_preview_user()) {
-            return false;
-        }
         $messages = [];
         $this->antiai_getsessioninfo();
         if($SESSION->quizaccess_antiai_access == 0){
             $messages = [html_writer::div(get_string('aiextensionfound','quizaccess_antiai'), 'alert alert-warning')];
         }
         return $messages;
-
     }
     public static function antiai_getsessioninfo() {
          global $PAGE;
