ba22959cd2277cdea01730f42a600bc5702d4159
Nexterday <nexterday@users.noreply.github.com>
2026-01-15 09:04:33 +0100

    Update plugin version to 2026011500

--- version.php
+++ version.php
@@ -27,6 +27,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2026011401;
+$plugin->version   = 2026011500;
 $plugin->requires  = 2024100100;
 $plugin->component = 'quizaccess_antiai';
